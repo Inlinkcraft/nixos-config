de30f84931693aef139c985e7c25a0c7faf219f7
Inlinkcraft <inlinkcraft@gmail.com>
2025-12-16 16:47:33 -0500

    Retour sur ma décision

--- flake.nix
+++ flake.nix
@@ -18,12 +18,7 @@
     
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
     in {
-
-      devShells.${system} = {
-        cpp = import ./devshells/cpp.nix { inherit pkgs; };
-      };
 
       nixosConfigurations = {
 
