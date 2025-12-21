ff38d936c01adbeb524e2744847f1a787f5a3152
Antoine <anmon98@ulaval.ca>
2025-12-21 05:29:11 -0500

    monitor fixing

--- hosts/pc/home.nix
+++ hosts/pc/home.nix
@@ -1,7 +1,10 @@
-wayland.windowManager.hyprland.settings = {
-  monitor = [
-    "DP-1,preferred,0x0,1"
-    "HDMI-1,preferred,1920x0,1"
-  ];
-};
+{ config, pkgs, ... }:
 
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-1,preferred,0x0,1"
+      "HDMI-1,preferred,1920x0,1"
+    ];
+  };
+}
