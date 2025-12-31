75d2aef1541bbcfe4171d156c87c1d4955d77e9a
Antoine <anmon98@ulaval.ca>
2025-12-31 15:52:45 -0500

    im tired of this

--- modules/eww/default.nix
+++ modules/eww/default.nix
@@ -1,27 +1,54 @@
 { config, pkgs, lib, ... }:
 
 let
-  ewwConfigDir = "%h/.config/eww";
+  ewwDir = "%h/.config/eww";
 
-  runtimePkgs = with pkgs; [
-    bash
-    eww
-    coreutils gawk gnugrep jq curl
-    procps lm_sensors
-    networkmanager
-    pamixer brightnessctl acpi
-    playerctl bluez
-    util-linux systemd
-    swaylock-effects
+  # Make sure scripts called by defpoll have what they need in PATH
+  pathBins = lib.makeBinPath [
+    pkgs.bash
+    pkgs.coreutils
+    pkgs.gnugrep
+    pkgs.gawk
+    pkgs.util-linux
+    pkgs.procps
+    pkgs.lm_sensors
+    pkgs.networkmanager
+    pkgs.brightnessctl
+    pkgs.playerctl
+    pkgs.pulseaudio
+    pkgs.pamixer
+    pkgs.acpi
+    pkgs.curl
+    pkgs.jq
+    pkgs.khal
+    pkgs.swaylock-effects
   ];
-
-  runtimePath = lib.makeBinPath runtimePkgs;
 in
 {
-  home.packages = runtimePkgs;
+  home.packages = with pkgs; [
+    eww
+    brightnessctl
+    playerctl
+    networkmanager
+    pamixer
+    acpi
+    curl
+    jq
+    khal
+    swaylock-effects
+    lm_sensors
+    procps
+  ];
 
-  home.file.".config/eww/eww.yuck" = { source = ./config/eww.yuck; force = true; };
-  home.file.".config/eww/eww.scss" = { source = ./config/eww.scss; force = true; };
+  home.file.".config/eww/eww.yuck" = {
+    source = ./config/eww.yuck;
+    force = true;
+  };
+
+  home.file.".config/eww/eww.scss" = {
+    source = ./config/eww.scss;
+    force = true;
+  };
 
   home.file.".config/eww/scripts/dashboard" = {
     source = ./config/scripts/dashboard;
@@ -29,8 +56,15 @@ in
     force = true;
   };
 
-  home.file.".config/eww/assets/avatar.svg" = { source = ./config/assets/avatar.svg; force = true; };
-  home.file.".config/eww/assets/cover.svg"  = { source = ./config/assets/cover.svg;  force = true; };
+  home.file.".config/eww/assets/avatar.svg" = {
+    source = ./config/assets/avatar.svg;
+    force = true;
+  };
+
+  home.file.".config/eww/assets/cover.svg" = {
+    source = ./config/assets/cover.svg;
+    force = true;
+  };
 
   systemd.user.services.eww = {
     Unit = {
@@ -42,10 +76,10 @@ in
     Service = {
       Type = "simple";
       Environment = [
-        "PATH=${runtimePath}:%h/.config/eww/scripts"
-        "EWW_CONFIG_DIR=%h/.config/eww"
+        "PATH=${pathBins}"
       ];
-      ExecStart = "${pkgs.eww}/bin/eww daemon --no-daemonize --config ${ewwConfigDir}";
+
+      ExecStart = "${pkgs.eww}/bin/eww daemon --no-daemonize --config ${ewwDir}";
       Restart = "on-failure";
       RestartSec = "1s";
     };
