4d685a605e8b62742970b85913ef251442680187
Antoine <anmon98@ulaval.ca>
2026-01-02 21:48:35 -0500

    test

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -16,6 +16,7 @@
 	nvim-treesitter-parsers.markdown
 	nvim-treesitter-parsers.markdown_inline
 	nvim-treesitter-parsers.cmake
+    nvim-treesitter-parsers.eww
     ];
     extraLuaConfig = with pkgs.vimPlugins; 
     "
